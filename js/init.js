32e735423f2d7cf188e9f4d632e3f779d68cc77c
axelcola <axelcola1@gmail.com>
2021-08-29 00:19:52 -0300

    Emercado JAP
    Ya tiene autentificación de google y todo lo necesario para la entrega 2

--- js/init.js
+++ js/init.js
@@ -39,20 +39,7 @@ var getJSONData = function(url){
         return result;
     });
 }
-  function onSignIn(googleUser) {
-        // Useful data for your client-side scripts:
-        var profile = googleUser.getBasicProfile();
-        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-        console.log('Full Name: ' + profile.getName());
-        console.log('Given Name: ' + profile.getGivenName());
-        console.log('Family Name: ' + profile.getFamilyName());
-        console.log("Image URL: " + profile.getImageUrl());
-        console.log("Email: " + profile.getEmail());
-
-        // The ID token you need to pass to your backend:
-        var id_token = googleUser.getAuthResponse().id_token;
-        console.log("ID Token: " + id_token);
-      }
+ 
 function showUser(){
   let user = localStorage.getItem("usuario");
   let htmlContentToAppend= `<a class="py-2 d-none d-md-inline-block" href="my-profile.html">` + user + `</a>`;
