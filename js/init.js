2bb84d527fec39c22a0f16ba234b43a80a453277
axelcola <axelcola1@gmail.com>
2021-08-29 16:31:16 -0300

    probando google 1

--- js/init.js
+++ js/init.js
@@ -52,6 +52,7 @@ function onSignIn(googleUser) {
       // The ID token you need to pass to your backend:
       var id_token = googleUser.getAuthResponse().id_token;
       console.log("ID Token: " + id_token);
+      localStorage.setItem("usuario", profile.getName());
       location.href = "Home.html";
     }
 function showUser() {
