c6f0687e9de6aa295dce9c7c8b6bb7b1dbb1ff43
axelcola <axelcola1@gmail.com>
2021-09-02 00:07:20 -0300

    Entrega 2 JAP + desafíate
    Funciona la curz del buscador

--- js/products.js
+++ js/products.js
@@ -7,29 +7,27 @@ var currentSortCriteria = undefined;
 var minCount = undefined;
 var maxCount = undefined;
 
-
-function sortProduct(criteria, array){
+function sortProduct(criteria, array) {
     let result = [];
-    if (criteria === ORDER_ASC_BY_PRICE)
-    {
-        result = array.sort(function(a, b) {
-            if ( a.cost < b.cost ){ return -1; }
-            if ( a.cost > b.cost ){ return 1; }
+    if (criteria === ORDER_ASC_BY_PRICE) {
+        result = array.sort(function (a, b) {
+            if (a.cost < b.cost) { return -1; }
+            if (a.cost > b.cost) { return 1; }
             return 0;
         });
-    }else if (criteria === ORDER_DESC_BY_PRICE){
-        result = array.sort(function(a, b) {
-            if ( a.cost > b.cost ){ return -1; }
-            if ( a.cost < b.cost ){ return 1; }
+    } else if (criteria === ORDER_DESC_BY_PRICE) {
+        result = array.sort(function (a, b) {
+            if (a.cost > b.cost) { return -1; }
+            if (a.cost < b.cost) { return 1; }
             return 0;
         });
-    }else if (criteria === ORDER_BY_PROD_COUNT){
-        result = array.sort(function(a, b) {
+    } else if (criteria === ORDER_BY_PROD_COUNT) {
+        result = array.sort(function (a, b) {
             let aCount = parseInt(a.soldCount);
             let bCount = parseInt(b.soldCount);
 
-            if ( aCount > bCount ){ return -1; }
-            if ( aCount < bCount ){ return 1; }
+            if (aCount > bCount) { return -1; }
+            if (aCount < bCount) { return 1; }
             return 0;
         });
     }
@@ -67,10 +65,10 @@ function showProductList() {
     }
 }
 
-function sortAndShowProduct(sortCriteria, ProductArray){
+function sortAndShowProduct(sortCriteria, ProductArray) {
     currentSortCriteria = sortCriteria;
 
-    if(ProductArray != undefined){
+    if (ProductArray != undefined) {
         currentProductArray = ProductArray;
     }
 
@@ -89,19 +87,19 @@ document.addEventListener("DOMContentLoaded", function (e) {
             sortAndShowProduct(ORDER_ASC_BY_PRICE, resultObj.data);
         }
     });
-    document.getElementById("sortAsc").addEventListener("click", function(){
+    document.getElementById("sortAsc").addEventListener("click", function () {
         sortAndShowProduct(ORDER_ASC_BY_PRICE);
     });
 
-    document.getElementById("sortDesc").addEventListener("click", function(){
+    document.getElementById("sortDesc").addEventListener("click", function () {
         sortAndShowProduct(ORDER_DESC_BY_PRICE);
     });
 
-    document.getElementById("sortByCount").addEventListener("click", function(){
+    document.getElementById("sortByCount").addEventListener("click", function () {
         sortAndShowProduct(ORDER_BY_PROD_COUNT);
     });
 
-    document.getElementById("clearRangeFilter").addEventListener("click", function(){
+    document.getElementById("clearRangeFilter").addEventListener("click", function () {
         document.getElementById("rangeFilterCountMin").value = "";
         document.getElementById("rangeFilterCountMax").value = "";
 
@@ -133,30 +131,30 @@ document.addEventListener("DOMContentLoaded", function (e) {
         showProductList(currentProductArray);
     });
 });
-document.getElementById("buscador").addEventListener("keyup", (event) => {
+document.getElementById("buscador").addEventListener("input", (event) => {
 
     let htmlContentToAppend = "";
     for (let i = 0; i < currentProductArray.length; i++) {
         let product = currentProductArray[i];
-        
-        if (product.name.toUpperCase().includes(document.getElementById("buscador").value.toUpperCase())){
-            
+
+        if (product.name.toUpperCase().includes(document.getElementById("buscador").value.toUpperCase())) {
+
             htmlContentToAppend += `
-            <div class="list-group-item list-group-item-action">
-                <div class="row">
-                    <div class="col-3">
-                        <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
-                    </div>
-                    <div class="col">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">` + product.name + `</h4><h4 style="text-align: right"> ` + product.currency + ` ` + product.cost + ` </h4>
-                            <small class="text-muted">` + product.soldCount + `artículos</small>
+                <div class="list-group-item list-group-item-action">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
                         </div>
-                        <p class="mb-1"> ` + product.description + `</p>
+                        <div class="col">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h4 class="mb-1">` + product.name + `</h4><h4 style="text-align: right"> ` + product.currency + ` ` + product.cost + ` </h4>
+                                <small class="text-muted">` + product.soldCount + `artículos</small>
+                            </div>
+                            <p class="mb-1"> ` + product.description + `</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            ` 
+                `
         }
         document.getElementById("listaDeProductos").innerHTML = htmlContentToAppend;
     }
