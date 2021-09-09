e4fbc7c97c3ed6effea2000066e87cf44d688d7d
axelcola <axelcola1@gmail.com>
2021-09-09 13:37:34 -0300

    imagenes relacionadas

--- js/product-info.js
+++ js/product-info.js
@@ -1,5 +1,6 @@
 var product = {};
 var comments = {};
+var related = {};
 function showImagesGallery(array) {
 
     let htmlContentToAppend = "";
@@ -8,18 +9,52 @@ function showImagesGallery(array) {
         let images = array[i];
 
         htmlContentToAppend += `
-        <a href="` + images + `"
-            <div class="col-lg-3 col-md-4 col-6">
+        <div class="col-lg-4 col-md-4 col-6">
+            <a href="` + images + `">
                 <div class="d-block mb-4 h-100">
                     <img class="img-fluid img-thumbnail" src="` + images + `" alt="">
                 </div>
-            </div>
-        </a>
+            </a>
+        </div>
+        
         `
 
         document.getElementById("productImagesGallery").innerHTML = htmlContentToAppend;
     }
 }
+function relatedProducts(array) {
+
+    let htmlContentToAppend = "";
+
+    for (let i = 0; i < array.length; i++) {
+        let product = array[i];
+
+        if (i < 2){   
+
+            htmlContentToAppend += `
+            <a href="products.html" class="list-group-item list-group-item-action">
+                <div class="list-group-item list-group-item-action">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+                        </div>
+                        <div class="col">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h4 class="mb-1">` + product.name + `</h4><h4 style="text-align: right"> ` + product.currency + ` ` + product.cost + ` </h4>
+                                <small class="text-muted">` + product.soldCount + `artículos</small>
+                            </div>
+                            <p class="mb-1"> ` + product.description + `</p>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        `
+
+            document.getElementById("relatedProducts").innerHTML = htmlContentToAppend;
+        }
+    }
+}
+
 function showComments(array) {
 
     let htmlContentToAppend = "";
@@ -66,9 +101,21 @@ document.addEventListener("DOMContentLoaded", function (e) {
 
             //Muestro las imagenes en forma de galería
             showImagesGallery(product.images);
+            }
+    });
+});
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            related = resultObj.data;
+
+            relatedProducts(related);
         }
     });
 });
+
+
+
 document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (resultObj) {
         if (resultObj.status === "ok") {
