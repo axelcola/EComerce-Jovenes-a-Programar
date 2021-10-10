7f206a38366511115c82f1e131054bb20794761e
axelcola <axelcola1@gmail.com>
2021-10-10 19:25:01 -0300

    funciona la calculadora pero no convierte

--- js/cart.js
+++ js/cart.js
@@ -2,36 +2,59 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 function showProductCart(productToCart) {
-      let array = productToCart.articles;
-      let htmlContentToAppend = "";
-      for (let i = 0; i < array.length; i++) {
-              let product = array[i];
-              htmlContentToAppend += `
-              
-                  <div class="list-group-item list-group-item-action">
-                      <div class="row">
-                          <div class="col-3">
-                              <img src="` + product.src + `" alt="` + product.name + `" class="img-thumbnail">
-                          </div>
-                          <div class="col">
-                              <div class="d-flex w-100 justify-content-between">
-                                  <h4 class="mb-1">` + product.name + `</h4><h4 style="text-align: right"> ` + product.currency + ` ` + product.unitCost+ ` </h4>
-                              </div>
-                              <p class="mb-1"> Cantidad "` + product.count + `"</p> 
-                              <small class="text-muted">` + product.soldCount + `artículos</small>
-                          </div>
-                      </div>
-                  </div>
-          `
-          document.getElementById("productsCart").innerHTML = htmlContentToAppend;
-      }
-  }
+    let array = productToCart.articles;
+    let htmlContentToAppend = "";
+    for (let i = 0; i < array.length; i++) {
+            let product = array[i];
+            htmlContentToAppend += `
+            
+                <div class="list-group-item list-group-item-action">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="` + product.src + `" alt="` + product.name + `" class="img-thumbnail">
+                        </div>
+                        <div class="col">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h4 class="mb-1">` + product.name + `</h4>
+                            </div>
+                            <div class="col-3">
+                              <input type="number" class="form-control" id="productCostInput`+i+`" onchange="calculadora(`+product.unitCost+`,`+ i +`)" placeholder="" required="" value="` + product.count + `" min="0">
+                            </div>  
+                        </div>
+                    </div>
+                </div>
+        `
+        document.getElementById("productsCart").innerHTML = htmlContentToAppend;
+    }
+}
+function showCalculator(productToCart) {
+    let array = productToCart.articles;
+    let htmlContentToAppend = "";
+    for (let i = 0; i < array.length; i++) {
+            let product = array[i];
+            htmlContentToAppend += `
+            
+                <div class="d-flex w-100 justify-content-between">
+                    <h4 style="text-align: right"> ` + product.currency +` <span id="costo`+i+`" >`+product.unitCost * product.count+`</span></h4>
+                </div>
+        `
+        document.getElementById("calculator").innerHTML = htmlContentToAppend;
+    }
+}
+function calculadora(product, i){
+  let identIput = "productCostInput"+i;
+  let identCosto = "costo"+i;
+  let result = document.getElementById(identIput).value * product;
+  document.getElementById(identCosto).innerHTML = result;
+}
 
-  document.addEventListener("DOMContentLoaded", function (e) {
-      getJSONData(CART_INFO_URL).then(function (resultObj) {
-          if (resultObj.status === "ok") {
-            productCart = resultObj.data
 
-              showProductCart(productCart);}
-          });
-      });
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(CART_INFO_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+          productCart = resultObj.data
+
+            showProductCart(productCart);
+            showCalculator(productCart);}
+        });
+    });
